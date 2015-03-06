907701e24307d88115c67f47f781b4f679ba8695
kevin <null386@gmail.com>
2015-03-07 00:53:46 +0800

    finish coding.

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+import config
+from dbutils import db
+
+app = Flask(__name__)
+
+def configure_app(app):
+    # 载入配置
+    app.config.from_object(config.__name__)
+
+    # 导入sql
+    #db.dump_sql(app)
+
+    @app.before_request
+    def before_request():
+        db.connect()
+
+    @app.teardown_request
+    def teardown_request(exception):
+        db.close()
+
+configure_app(app)
--- dbutils.py
+++ dbutils.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+import config
+
+class DBUtils(object):
+    connection = None
+
+    def __init__(self):
+        pass
+
+    def dump_sql(self, app):
+        self.connect()
+        with app.open_resource('schema.sql') as f:
+            self.connection.cursor().executescript(f.read())
+        self.connection.commit()
+        self.close()
+
+    def connect(self):
+        self.connection = sqlite3.connect(config['DATABASE'])
+
+    def close(self):
+        self.connection.close()
+
+db = DBUtils()
+
+
+
+
